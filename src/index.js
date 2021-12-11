59ca680b549743b229ea9a55a9cef7550077a406
Avinash V K <avinash2000vk@gmail.com>
2021-12-11 12:42:48 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -1,28 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 8000
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = 5050;
 const extractText = require("./utils/extractText");
 
-app.get('/', (req, res) => {
-  res.send('Service running successfully!');
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Service running successfully!");
 });
 
-app.get('/ping', (req, res) => {
-  res.send('pong');
+app.get("/ping", (req, res) => {
+  res.send("pong");
 });
 
-app.get('/extract-text', async (req, res) => {
+app.get("/extract-text", async (req, res) => {
   try {
     const { url } = req.query;
     let extractedText = await extractText(url);
     res.status(200).json({
       data: {
-        text: extractedText
-      }
+        text: extractedText,
+      },
     });
-  } catch(err) {
+  } catch (err) {
     res.status(500).send({
-      error: err.message
+      error: err.message,
     });
   }
 });
